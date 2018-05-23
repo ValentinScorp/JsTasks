6e2ae17434ceae438d7da7ceece76b5a7e8ce172
Valentin Bloschinskiy <valentin.blo@gmail.com>
2018-05-23 16:54:23 +0300

    Task4 edit

--- Task4/demo.js
+++ Task4/demo.js
@@ -68,15 +68,90 @@ scoder2.add(null);
 // Создать класс, который имеет 3 ф-ции (create, increment, delete_lastItem). Каждая ф-ция имеет прикреплена к кнопке на странице (create, inc, del соответственно).
 // Create создает в цикле 20 элементов и прикрепяет их к странице. Каждый элемент представляет из себя блок с 2мя кнопками и числом внутри блока. 1ая кнопка "+1" инкрементирует значение блока на +1. 2ая кнопка "х" удаляет элемент со страницы перед этим выводя индекс на каком был создан компонент. Каждый блок имеет начальное значение равное значению индекса во время какого блок создавался
 
+function createButton(parent, text) {
+	var b1 = document.createElement("button");
+	var b1text = document.createTextNode(text);
+	b1.appendChild(b1text);
+	parent.appendChild(b1);
+	return b1;
+}
+function createText(parent, text) {
+	var textelem = document.createTextNode(text);
+	parent.appendChild(textelem);
+	return textelem;
+}
+
+function Block(index) {
+	var block = this;
+	this.initIndex = index;
+	this.currentIndex = index;
+
+	this.divelem = document.createElement("div");
+	this.divelem.id = "div" + String(index);
+	this.incbuttonelem = createButton(this.divelem, "+1");
+	this.delbuttonelem = createButton(this.divelem, "X");
+	this.texelem = createText(this.divelem, this.currentIndex);
+
+	this.increaseIndex = function() {
+		block.currentIndex += 1;
+		block.texelem.nodeValue = String(block.currentIndex);
+	}
+	this.incbuttonelem.onclick = function() {
+		block.increaseIndex();
+	};
+
+	this.delbuttonelem.onclick = function() {
+		alert(String(block.initIndex));
+		document.body.removeChild(block.divelem);
+	};
+	this.setIndex = function(newInd) {
+		block.currentIndex = newInd;
+		block.texelem.nodeValue = String(block.currentIndex);
+	}
+	document.body.appendChild(this.divelem);
+}
+
+function getElementsNum(curElem) {
+	var parent = curElem.parentNode;
+    var children = parent.children;
+	return children.length;
+}
+
+function getElementIndex(curElem) {
+	var parent = curElem.parentNode;
+	var ind = 0;
+	alert(parent.children.length);
+	for (var i = 0; i < parent.children.length; i++) {
+		if (parent.children[i].id == curElem.id) {
+			return i;
+		}
+		ind += 1;
+	}
+	return null;
+}
+
 function MainClass() {
+	var mainclass = this;
+	this.blocks = [];
+	this.blocksCounter = 0;
+
 	this.create = function() {
-		var b1 = document.createElement("button");
+		for (var i = 0; i < 20; i++) {
+			mainclass.blocks.push(new Block(i));
+		}
 	}
 	this.increment = function() {
-		alert("increment");
+		var elnum = getElementsNum(mainclass.blocks[0].divelem);
+		for (var b in mainclass.blocks) {
+			var div = mainclass.blocks[b].divelem;
+			if (typeof(div) == "object") {
+				var index = getElementIndex(div);
+				mainclass.blocks[b].setIndex(parseInt(index) + parseInt(elnum));
+			}
+		}
 	}
 	this.delete_lastItem = function() {
-
+		document.body.removeChild(document.body.lastChild);
 	}
 }
 
